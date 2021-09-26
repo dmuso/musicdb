ea51ff29c94e3e848dd6a288bd4bb1076d7d9664
Dan Harper <dan@kingdomsolutions.com.au>
2021-09-26 12:28:01 +1000

    Make tests work with new FK constraints

--- piece/tests.py
+++ piece/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
-from .models import Piece, Category, Instrument
+from .models import InstrumentGroup, Location, Piece, Category, Instrument, Publisher
 
 class PieceTestCase(TestCase):
   def setUp(self):
     Category.objects.create(name="AMEB", code="01")
-    Instrument.objects.create(name="Flute", abbreviation="FL")
+    instrument_group = InstrumentGroup.objects.create(name="Woodwind")
+    Instrument.objects.create(name="Flute", abbreviation="FL", instrument_group=instrument_group)
+    Publisher.objects.create(name="Test Publisher")
+    Location.objects.create(name="Test Location")
 
   def test_suggested_cat_number_returns_empty_with_no_instrument_selected(self):
     piece_first = Piece()
@@ -13,20 +16,29 @@ class PieceTestCase(TestCase):
   def test_suggested_cat_number_instrument_category_prefixes(self):
     category = Category.objects.get(name="AMEB")
     instrument = Instrument.objects.get(name="Flute")
-    piece_first = Piece(category=category, instrument=instrument)
+    publisher = Publisher.objects.get(name="Test Publisher")
+    location = Location.objects.get(name="Test Location")
+
+    piece_first = Piece(category=category, instrument=instrument, publisher=publisher, location=location)
     self.assertEqual(piece_first.suggested_cat_number(), instrument.abbreviation + category.code + ".0001")
 
   def test_suggested_cat_number_starts_at_one(self):
     category = Category.objects.get(name="AMEB")
     instrument = Instrument.objects.get(name="Flute")
-    piece_first = Piece(category=category, instrument=instrument)
+    publisher = Publisher.objects.get(name="Test Publisher")
+    location = Location.objects.get(name="Test Location")
+
+    piece_first = Piece(category=category, instrument=instrument, publisher=publisher, location=location)
     self.assertEqual(piece_first.suggested_cat_number(), instrument.abbreviation + category.code + ".0001")
 
   def test_suggested_cat_number_increments(self):
     category = Category.objects.get(name="AMEB")
     instrument = Instrument.objects.get(name="Flute")
-    piece_first = Piece.objects.create(category=category, instrument=instrument, catalogue_number=instrument.abbreviation + category.code + ".0001")
-    piece_second = Piece(category=category, instrument=instrument)
+    publisher = Publisher.objects.get(name="Test Publisher")
+    location = Location.objects.get(name="Test Location")
+
+    piece_first = Piece.objects.create(category=category, instrument=instrument, catalogue_number=instrument.abbreviation + category.code + ".0001", publisher=publisher, location=location)
+    piece_second = Piece(category=category, instrument=instrument, publisher=publisher, location=location)
     self.assertEqual(piece_second.suggested_cat_number(), instrument.abbreviation + category.code + ".0002")
 
   def test_last_cat_number_returns_empty_with_no_instrument_selected(self):
@@ -36,7 +48,10 @@ class PieceTestCase(TestCase):
   def test_last_cat_number(self):
     category = Category.objects.get(name="AMEB")
     instrument = Instrument.objects.get(name="Flute")
-    piece_first = Piece.objects.create(category=category, instrument=instrument, catalogue_number=instrument.abbreviation + category.code + ".0001")
-    piece_second = Piece(category=category, instrument=instrument)
+    publisher = Publisher.objects.get(name="Test Publisher")
+    location = Location.objects.get(name="Test Location")
+
+    piece_first = Piece.objects.create(category=category, instrument=instrument, catalogue_number=instrument.abbreviation + category.code + ".0001", publisher=publisher, location=location)
+    piece_second = Piece(category=category, instrument=instrument, publisher=publisher, location=location)
     self.assertEqual(piece_second.last_cat_number(), instrument.abbreviation + category.code + ".0001")
 
